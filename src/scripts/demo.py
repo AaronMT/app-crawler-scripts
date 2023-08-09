4086ca8a9c7414ed82b95cb4e3d7d85f05fed2d7
Aaron Train <aaron.train@gmail.com>
2023-08-09 16:16:41 -0400

    Adjust arguments in demo.py

--- src/scripts/demo.py
+++ src/scripts/demo.py
@@ -8,7 +8,7 @@ def run_crawler(jar_path, app_path, sdk_path, script, timeout):
         "--apk-file", app_path,
         "--android-sdk", sdk_path,
         "--robo-script-file", script,
-        "--timeout", timeout
+        "--timeout-sec", timeout
     ]
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
@@ -24,7 +24,7 @@ def main():
     parser.add_argument('--jar', required=True, help='Path to the crawler JAR file.')
     parser.add_argument('--app', required=True, help='Path to the APK file of the app to crawl.')
     parser.add_argument('--sdk', required=True, help='Path to the Android SDK.')
-    parser.add_argument('--script', required=False, help='Path to the Robo Script.')
+    parser.add_argument('--script', required=True, help='Path to the Robo Script.')
     parser.add_argument('--timeout', default=60, help='Timeout in seconds for the crawler.')
 
     args = parser.parse_args()
