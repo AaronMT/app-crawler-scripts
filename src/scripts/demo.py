af4a7a5125b192ba63d7487e72858ce336e8f05e
Aaron Train <aaron.train@gmail.com>
2023-08-30 13:08:58 -0400

    Updated demo and fetch-crawler

--- src/scripts/demo.py
+++ src/scripts/demo.py
@@ -8,7 +8,7 @@ def run_crawler(jar_path, app_path, sdk_path, script, timeout):
         "--apk-file", app_path,
         "--android-sdk", sdk_path,
         "--robo-script-file", script,
-        "--timeout-sec", timeout
+        "--timeout-sec", str(timeout)
     ]
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
@@ -22,14 +22,14 @@ def run_crawler(jar_path, app_path, sdk_path, script, timeout):
 def main():
     parser = argparse.ArgumentParser(description='Run Android App Crawler.')
     parser.add_argument('--jar', required=True, help='Path to the crawler JAR file.')
-    parser.add_argument('--app', required=True, help='Path to the APK file of the app to crawl.')
-    parser.add_argument('--sdk', required=True, help='Path to the Android SDK.')
-    parser.add_argument('--script', required=True, help='Path to the Robo Script.')
-    parser.add_argument('--timeout', default=60, help='Timeout in seconds for the crawler.')
+    parser.add_argument('--apk-file', required=True, help='Path to the APK file of the app to crawl.')
+    parser.add_argument('--android-sdk', required=True, help='Path to the Android SDK.')
+    parser.add_argument('--robo-script-file', required=True, help='Path to the Robo Script.')
+    parser.add_argument('--timeout-sec', default=60, help='Timeout in seconds for the crawler.')
 
     args = parser.parse_args()
 
-    run_crawler(args.jar, args.app, args.sdk, args.script, args.timeout)
+    run_crawler(args.jar, args.apk_file, args.android_sdk, args.robo_script_file, args.timeout_sec)
 
 
 if __name__ == "__main__":
--- src/scripts/fetch-crawler.sh
+++ src/scripts/fetch-crawler.sh
@@ -12,6 +12,21 @@ if curl --output /dev/null --silent --head --fail "$url"; then
   echo "Downloading file..."
   curl --silent --location --output "$output_file" "$url"
   echo "Download completed. File saved as $output_file"
+
+  # Extract the zip file to the current directory
+  echo "Extracting zip file..."
+  unzip "$output_file"
+  echo "Extraction completed."
+
+  # Move the contents of the app-crawler folder to the current directory
+  echo "Moving contents of app-crawler folder to current directory..."
+  mv app-crawler/* .
+  echo "Moved contents of app-crawler folder to current directory."
+
+  # Remove the app-crawler folder
+  echo "Removing app-crawler folder..."
+  rmdir app-crawler
+  echo "Removed app-crawler folder."
 else
   echo "URL does not exist: $url"
 fi
